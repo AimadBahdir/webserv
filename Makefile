fd8668cf033dd30912390b61ffab0684c38e6d02
Aimad Bahdir <abahdir@e2r10p10.1337.ma>
2022-02-17 11:31:48 +0100

    .

---
+++ Makefile
@@ -0,0 +1,19 @@
+NAME = webserv
+SRC = main.cpp responder/*/*.cpp
+FLAGS = -Wall -Wextra -Werror -std=c++98
+
+all: $(NAME)
+
+$(NAME):
+	@clang++ $(FLAGS) $(SRC) -o $(NAME)
+
+clean:
+	@rm -rf $(NAME)
+
+fclean: clean
+
+re: fclean all
+
+r: re
+	@clear
+	@./$(NAME)
---
+++ main.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <map>
+#include <vector>
+
+struct location
+{
+    std::string path;
+    std::string root;
+    std::string cgi;
+    std::vector<std::string> index;
+    bool auto_index;
+};
+
+struct server
+{
+    std::vector<std::string> names;
+    std::string host;
+    std::string port;
+    std::vector<std::string> error_page;
+    std::vector<location> locations;
+};
+
+int main()
+{
+    std::map<std::string, std::string> requestData;
+
+    requestData.insert(std::make_pair("Methode", "GET"));
+    requestData.insert(std::make_pair("Path", "/"));
+    requestData.insert(std::make_pair("Version", "HTTP/1.1"));
+    requestData.insert(std::make_pair("Methode", "GET"));
+    requestData.insert(std::make_pair("Host", "127.0.0.1"));
+    requestData.insert(std::make_pair("Port", "8000"));
+    requestData.insert(std::make_pair("Connection", "keep-alive"));
+    requestData.insert(std::make_pair("Cache-Control", "max-age=0"));
+    requestData.insert(std::make_pair("Upgrade-Insecure-Requests", "1"));
+    requestData.insert(std::make_pair("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36"));
+    requestData.insert(std::make_pair("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"));
+    requestData.insert(std::make_pair("Sec-Fetch-Site", "cross-site"));
+    requestData.insert(std::make_pair("Sec-Fetch-Mode", "navigate"));
+    requestData.insert(std::make_pair("Sec-Fetch-User", "?1"));
+    requestData.insert(std::make_pair("Sec-Fetch-Dest", "document"));
+    requestData.insert(std::make_pair("Accept-Encoding", "gzip, deflate, br"));
+    requestData.insert(std::make_pair("Accept-Language", "en-US,en;q=0.9"));
+
+    std::cout << requestData["Methode"] << std::endl;
+
+    std::vector<server> servers;
+    
+    server srv1;
+    srv1.host = "0.0.0.0";
+    srv1.port = "8000";
+    srv1.names.push_back("server1");
+    srv1.names.push_back("server2");
+    srv1.names.push_back("server3");
+    srv1.error_page.push_back("500");
+    srv1.error_page.push_back("501");
+    srv1.error_page.push_back("502");
+    srv1.error_page.push_back("./50x.html");
+
+    location loc1;
+    loc1.path = "/home";
+    loc1.auto_index = false;
+    loc1.cgi = "/Users/etc/cgi";
+    loc1.index.push_back("index.html");
+    loc1.index.push_back("index.htm");
+    loc1.root = "/Users/home/";
+
+    location loc2;
+    loc2.path = "/about";
+    loc2.auto_index = true;
+    loc2.cgi = "/Users/etc/cgi";
+    loc2.index.push_back("index.html");
+    loc2.index.push_back("index.htm");
+    loc2.root = "/Users/about/";
+
+    srv1.locations.push_back(loc1);
+    srv1.locations.push_back(loc2);
+    servers.push_back(srv1);
+
+    
+
+    return (0);
+}
