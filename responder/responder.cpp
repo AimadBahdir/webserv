b3f843e3384febd4e53657665044f8cc8b5252af
Aimad Bahdir <abahdir@e2r11p10.1337.ma>
2022-02-19 15:59:29 +0100

    .

--- responder/responder.cpp
+++ responder/responder.cpp
@@ -1,8 +1,6 @@
 #include "./responder.hpp"
 
-Responder::Responder(servers_vec servers) : _servers(servers)
-{
-}
+Responder::Responder(servers_vec servers) : _servers(servers) {}
 
 Responder::Responder(Responder const & r)
 {
@@ -15,9 +13,7 @@ Responder& Responder::operator=(Responder const & r)
     return (*this);
 }
 
-Responder::~Responder()
-{
-}
+Responder::~Responder() {}
 
 std::string Responder::Response(request_map request)
 {
@@ -25,4 +21,32 @@ std::string Responder::Response(request_map request)
     return (request["URI"]);
 }
 
+std::string Responder::CGI_Response() 
+{
+    return ("Res");
+}
 
+std::string Responder::GET_Methode() 
+{
+    return ("Res");
+}
+
+std::string Responder::POST_Methode() 
+{
+    return ("Res");
+}
+
+std::string Responder::DELTE_Methode() 
+{
+    return ("Res");
+}
+
+std::string Responder::UploadFile() 
+{
+    return ("Res");
+}
+
+std::string Responder::GetErrorPage() 
+{
+    return ("Res");
+}
--- responder/responder.hpp
+++ responder/responder.hpp
@@ -15,10 +15,10 @@ class Responder
         std::string Response(request_map);
     private:
         servers_vec _servers;
-        // std::string CGI_Response();
-        // std::string GET_Methode();
-        // std::string POST_Methode();
-        // std::string DELTE_Methode();
-        // std::string UploadFile();
-        // std::string GetErrorPage();
+        std::string CGI_Response();
+        std::string GET_Methode();
+        std::string POST_Methode();
+        std::string DELTE_Methode();
+        std::string UploadFile();
+        std::string GetErrorPage();
 };
