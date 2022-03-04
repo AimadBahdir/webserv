590d8f3e4cb337540a1a5d994f98ede6df7852b0
Aimad Bahdir <56967395+ABLEET@users.noreply.github.com>
2022-03-04 11:58:08 +0100

    Update configStructs.cpp

--- configStructs.cpp
+++ configStructs.cpp
@@ -5,22 +5,35 @@
 
 struct location
 {
-    std::vector<std::string> accepted_requests;
-    std::string location_path;
-    std::string root_path;
-    std::vector<std::string> index;
-    std::string upload_path;
-    std::string cgi_path;
+    std::vector<std::string>    accepted_requests;
+    std::string                 location_path;
+    std::string                 root_path;
+    std::vector<std::string>    index;
+    std::string                 upload_path;
+    std::string                 cgi_path;
     std::pair<int, std::string> redirection;
-    bool auto_index;
+    bool                        auto_index;
 };
 
 struct server
 {
-    std::string host;
-    size_t port;
-    std::vector<std::string> names;
-    std::vector<std::string> error_pages;
-    std::vector<location> locations;
-    int clientMaxBodySize;
+    int                         port;
+    std::string                 host;
+    std::string                 root_path;
+    std::vector<std::string>    names;
+    std::vector<std::string>    error_pages;
+    int                         clientMaxBodySize;
+    std::vector<location>       locations;
+};
+
+struct request
+{
+    std::string                         error;
+    server                              serv;
+    std::string                         methode;
+    std::string                         path;
+    std::string                         queries;
+    std::string                         version;
+    std::map<std::string, std::string>  headers;
+    std::string                         bodyFile;
 };
